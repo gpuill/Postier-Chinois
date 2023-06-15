72e331e6b180b7ce2ef10481873c90010c82a314
Rochpearse <116382781+gpuill@users.noreply.github.com>
2023-06-15 17:30:13 +0200

    Add files via upload

---
+++ postier.py
@@ -0,0 +1,59 @@
+#Def graphe eulerien : Un graphe connexe admet un parcours eulérien si et seulement si ses sommets sont tous de degré pair sauf au plus deux
+import mygraph as mg
+graphe = {"A":["B","C","D","E"],
+          "B":["A","C","D","E"],
+          "C":["A","B","D","E"],
+          "D":["A","B","C","E"],
+          "E":["A","B","C","D"]}
+
+g = mg.Graphe(graphe)
+
+
+
+
+def comparer_Chemin(chem1,chem2):
+    if chem1==chem2[::-1] or chem1==chem2:
+        return True
+    else:
+        return False
+    
+def ifin(liste,arrete):
+    existe=False
+    i=0
+    while i<len(liste) and not existe:
+        if(comparer_Chemin(arrete,liste[i])):
+            return True
+        i+=1
+    return False
+
+def chemin_recursif(chemin,dep,nbMax,g):
+    liste=None
+    if len(chemin)==nbMax:
+        return chemin
+    for i in g.aretes(dep):
+        if not ifin(chemin,dep+i):
+            chemin.append(dep+i)
+            liste=chemin_recursif(chemin,i,nbMax,g)
+        elif i==g.aretes(dep)[-1]:
+            if len(chemin)!=nbMax:
+                chemin.pop()
+            return chemin
+        
+
+def chemin_Eulerien(g):
+    impair=[]
+    for som in g.all_sommets():
+        if len(g.aretes(som))%2==1:
+            impair.append(som)
+    if len(impair)>2:
+        return []
+    elif len(impair)>0 and len(impair)<=2:
+        dep=impair[0]
+    else:
+        dep=g.all_sommets()[0]
+    chemin=[]
+    nb=len(g.all_aretes())
+    chemin=chemin_recursif(chemin,dep,nb,g)
+    return chemin
+
+print(chemin_Eulerien(g))
